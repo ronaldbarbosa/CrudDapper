a42bd35e0ac1e5d624381e2e6b4383d9c85ef2dc
Ronald Barbosa <ronaldverissimo@hotmail.com>
2024-11-05 19:24:22 -0300

    Configured CORS

--- Api/Program.cs
+++ Api/Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.AddServices();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -14,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CORSPolicy");
 app.MapEndpoints();
 app.UseHttpsRedirection();
 
